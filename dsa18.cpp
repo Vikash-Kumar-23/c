ba0dfb8dba557393a5b2a015383fb9eb86dbb5e9
Vikash Kumar <vikashsio793@gmail.com>
2024-11-02 07:37:07 +0530

    adding new function

--- dsa18.cpp
+++ dsa18.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 void input(int arr[],int size){
     for (int i=0;i<size;i++){
@@ -7,9 +8,29 @@ void input(int arr[],int size){
     }
 }
 
-int output(int arr[],int size){
+int max(int arr[],int size){
+    int a=INT_MIN;
     for (int i=0;i<size;i++){
-        cout<<"arry :-"<<arr[i];
+        if (arr[i]>a){
+            a=arr[i];
+        }
+    }
+    return a;
+}
+
+int min(int arr[],int size){
+    int x=INT_MAX;
+    for (int i=0;i<size;i++){
+        if (arr[i]<x){
+            x=arr[i];
+        }
+    }
+    return x;
+}
+
+void output(int arr[],int size){
+    for (int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
     }
 }
 
@@ -20,5 +41,7 @@ int main(){
     cin>>size;
     input(n,size);
     output(n,size);
+    cout<<max(n,size)<<endl;
+    cout<<min(n,size);
     return 0;
 }
